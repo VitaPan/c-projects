fb245547bf959900ac1dfa10d34e41729f3666ba
Vita <klats.klatsss@gmail.com>
2023-01-09 20:07:56 +0700

    Finished Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+using System.Text;
+  Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var enc1251 = Encoding.GetEncoding(1251);
+ 
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+            System.Console.InputEncoding = enc1251;
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Число не имеет третьей цифры");
+}
+
+while (number > 999)
+{
+        number = number / 10;
+}
+Console.WriteLine($"Третья цифра заданного числа => {number % 10}");
