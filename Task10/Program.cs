a55fee56ec7971ff05d4f30ae69815924ab59dcb
Vita <klats.klatsss@gmail.com>
2023-01-09 19:54:52 +0700

    Finished task 10-

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,14 +5,8 @@
 // 782 -> 8
 // 918 -> 1
 
-using System.Text; // Добавила 8-14 строчки чтобы у меня терминал видел русский язык
+using System.Text; // Добавила строчку чтобы у меня терминал видел русский язык
 
-Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            var enc1251 = Encoding.GetEncoding(1251);
- 
-            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
-            System.Console.InputEncoding = enc1251;
-            
 Console.WriteLine("Введите трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
 
