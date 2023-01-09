d419f4ff019f6a12192ebfc5dfe2b02748249486
Vita <klats.klatsss@gmail.com>
2023-01-09 19:53:48 +0700

    Finished task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,11 +5,19 @@
 // 782 -> 8
 // 918 -> 1
 
+using System.Text; // Добавила 8-14 строчки чтобы у меня терминал видел русский язык
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var enc1251 = Encoding.GetEncoding(1251);
+ 
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+            System.Console.InputEncoding = enc1251;
+            
 Console.WriteLine("Введите трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int twoFirstDigits = number / 10;
-int secondDigit = twoFirstDigits % 10
+int secondDigit = twoFirstDigits % 10;
 
 if (number < 1000 && number > 99)
 {
